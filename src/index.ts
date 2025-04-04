2bd513b7d6c22a5f9a27a4be19daf639a12e2057
Pritam Debnath <pritamdebnath297@gmail.com>
2025-04-04 17:45:37 +0530

    initialised the project

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+import cors from "cors";
+import bodyParser from "body-parser";
+// import router from "./routes/index.js";
+
+const port = 3000;
+app.use(cors());
+
+app.use(bodyParser.json());
+
+// app.use("/api/v1", router);
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port} in http://localhost:${port}`);
+});
