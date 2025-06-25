e7d90803c92623f0c8898ee8d6d410421cf5ee77
Pritam Debnath <pritamdebnath297@gmail.com>
2025-06-25 12:18:44 +0530

    python process will be cleared more

--- news-worker/app/main.py
+++ news-worker/app/main.py
@@ -1,18 +1,16 @@
 # app/main.py
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, BackgroundTasks, Depends
-from app.database import create_db_and_tables, get_db
+
 from app.tasks.email_reader import process_gmail_alerts
-from app.models import User, Topic, UserTopicLink  # Import your models
-from sqlmodel import Session
+
+
 from app.routers import users as users_router, emails as emails_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Code to run on startup
     print("Starting up...")
-    await create_db_and_tables()
-    print("Database and tables created.")
     yield
     # Code to run on shutdown
     print("Shutting down...")
