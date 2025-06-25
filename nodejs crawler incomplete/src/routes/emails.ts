eaa951862cf6ace29978e20bfaa4becf1deb3137
Pritam Debnath <pritamdebnath297@gmail.com>
2025-06-25 12:16:18 +0530

    cleaning the project

--- nodejs crawler incomplete/src/routes/emails.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const emailRouter = express.Router();
-
-emailRouter.get("/", (req, res) => {
-    res.send("Hello from email route");
-})
-
-export default emailRouter;
--- nodejs crawler incomplete/src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import emailRouter from "./emails";
-const router = express.Router();
-
-router.use("/getFromEmail", emailRouter)
-
-export default router;
