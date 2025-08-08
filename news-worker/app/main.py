79c3057ec8e4bdbdbd664c5351970588b81907e8
Pritam Debnath <pritamdebnath297@gmail.com>
2025-08-08 19:22:31 +0530

    Revert "fix: when process starts fetch the articles"
    This reverts commit dafafa1e96f284c20675816447a2311f76b3dedd.

--- news-worker/app/main.py
+++ news-worker/app/main.py
@@ -1,6 +1,4 @@
 # app/main.py
-import redis
-from datetime import datetime
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, BackgroundTasks, Depends
 
@@ -14,22 +12,7 @@ from fastapi.middleware.cors import CORSMiddleware
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Code to run on startup
-    print("Starting up.....")
-    redisDB = redis.Redis(host='localhost', port=6379, db=0)
-    today_str = datetime.utcnow().strftime("%Y-%m-%d")
-    print(f"Redis connection established. Today's date: {today_str}")
-    last_run = redisDB.get("last_gmail_alert_check")
-    if last_run is not None:
-        last_run = last_run.decode('utf-8')
-    else:
-        last_run = None
-    if last_run != today_str:
-        print("Running Gmail alert processor...")
-        await process_gmail_alerts()
-        redisDB.set("last_gmail_alert_check", today_str)
-    else:
-        print("Gmail alerts already processed today.")
-
+    print("Starting up...")
     yield
     # Code to run on shutdown
     print("Shutting down...")
@@ -46,14 +29,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-
 app.include_router(users_router.router)
 app.include_router(emails_route.router)
 
-# @app.on_event("startup")
-# async def startup_event():
-    
 @app.get("/")
 def read_root():
     return {"message": "FastAPI is running 🚀"}
